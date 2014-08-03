d2436ee2532a62a61079535c45e72e12826db16e
Rahul makhija <rahulmkhj@gmail.com>
2014-08-03 17:46:24 +0530

    fixed constant conflicts

--- laravel/app/constants.php
+++ laravel/app/constants.php
@@ -1,6 +1,13 @@
 <?php
 
+if( ! defined('MB') )
 define('MB', 1048576);
+
+if( ! defined('GB') )
 define('GB', 1073741824);
+
+if( ! defined('Mins') )
 define('Mins', 60);
+
+if( ! defined('Hrs') )
 define('Hrs', 3600);
