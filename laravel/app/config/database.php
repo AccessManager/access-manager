1776199392a5a24f10e27a49ab07dc6148255d5d
Rahul makhija <rahulmkhj@gmail.com>
2014-11-10 15:10:11 +0530

    added database migration & model for direcpay payment gateway.

--- laravel/app/config/database.php
+++ laravel/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'acmanager',
+			'database'  => 'am-laravel',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
