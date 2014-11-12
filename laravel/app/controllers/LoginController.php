74b3a5d6c790a797a763dba2d4d183bc37e952bb
Rahul makhija <rahulmkhj@gmail.com>
2014-11-12 14:38:54 +0530

    Added “postInternetLogin” and “postAuthorizeInternetLogin” methods to loginController.php for hosted custom login page.

--- laravel/app/controllers/LoginController.php
+++ laravel/app/controllers/LoginController.php
@@ -38,4 +38,44 @@ Class LoginController extends AdminBaseController {
 		return Redirect::back()->withInput();
 	}
 
+	public function postInternetLogin()
+	{
+		Session::flash( 'mac', 			Input::get('mac', 				NULL) );
+		Session::flash( 'ip', 			Input::get('ip', 				NULL) );
+		Session::flash( 'username', 	Input::get('username', 			NULL) );
+		Session::flash( 'linklogin', 	Input::get('link-login', 		NULL) );
+		Session::flash( 'linkorig', 	Input::get('link-orig', 		NULL) );
+		Session::flash( 'chapid', 		Input::get('chap-id', 			NULL) );
+		Session::flash( 'chapchallenge',Input::get('chap-challenge', 	NULL) );
+		Session::flash( 'linkloginonly',Input::get('link-login-only', 	NULL) );
+		Session::flash( 'linkorigesc', 	Input::get('link-orig-esc', 	NULL) );
+		Session::flash( 'macesc', 		Input::get('mac-esc', 			NULL) );
+
+		$error = Input::get('error', NULL);
+		return View::make('login-template')
+					->with('error', $error);
+	}
+
+	public function postAuthorizeInternetLogin()
+	{
+		$data = [];
+
+		$data['mac'] 				= Session::get('mac');
+		$data['ip'] 				= Session::get('ip');
+		$data['linklogin'] 			= Session::get('linklogin');
+		$data['linkorig'] 			= Session::get('linkorig');
+		$data['chapid'] 			= Session::get('chapid');
+		$data['chapchallenge'] 		= Session::get('chapchallenge');
+		$data['linkloginonly'] 		= Session::get('linkloginonly');
+		$data['linkorigesc'] 		= Session::get('linkorigesc');
+		$data['macesc'] 			= Session::get('macesc');
+		$data['username']			= Input::get('username' );
+		$data['password']			= Input::get('password');
+
+		//Do All Authorization stuff here.
+
+		return View::make('internet-authorized')
+						->with('data', $data);
+	}
+
 }
