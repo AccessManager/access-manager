b32d11975172835c5104fec6e5ee14b1308427d7
Rahul makhija <rahulmkhj@gmail.com>
2014-11-20 13:31:44 +0530

    added self_signup and allow_free_ppp fields to fillable array of GeneralSettings model.

--- laravel/app/models/admin/GeneralSettings.php
+++ laravel/app/models/admin/GeneralSettings.php
@@ -2,6 +2,6 @@
 
 Class GeneralSettings extends BaseModel {
 	protected $table = 'general_settings';
-	protected $fillable = ['idle_timeout',];
+	protected $fillable = ['idle_timeout','self_signup','allow_free_ppp'];
 	public $timestamps = false;
 }
