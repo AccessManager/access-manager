e04c1144d23e0448b2b8bff6bd952af2454be9e8
Rahul makhija <rahulmkhj@gmail.com>
2014-12-09 18:38:08 +0530

    added new file OnlineRechargeController.php

---
+++ laravel/app/controllers/user/OnlineRechargeController.php
@@ -0,0 +1,22 @@
+<?php
+
+class OnlineRechargeController extends BaseUserController {
+
+
+	public function selectPaymentGateway()
+	{
+		
+	}
+	
+	public function validityRechargeOnline($plan_id)
+	{
+		$user_id = Auth::id();
+		
+	}
+
+	public function refillOnline()
+	{
+		
+	}
+}
+//end of file OnlineRechargeController.php
