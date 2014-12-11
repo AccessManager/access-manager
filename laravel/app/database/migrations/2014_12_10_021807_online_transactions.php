127d86db26e513dafab98dc0ac2864525d3fa119
Rahul makhija <rahulmkhj@gmail.com>
2014-12-11 16:23:06 +0530

    added new migration: online transactions

---
+++ laravel/app/database/migrations/2014_12_10_021807_online_transactions.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class OnlineTransactions extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('online_transactions', function(Blueprint $t){
+			$t->engine = 'InnoDB';
+			
+			$t->increments('id');
+			$t->integer('user_id');
+			$t->string('gw_type');
+			$t->integer('gw_id');
+			$t->float('amount');
+			$t->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('online_transactions');
+	}
+
+}
