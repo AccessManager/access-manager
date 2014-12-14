c28eb6a866b0fdd4f141e88d7c8e0f8f01775202
Rahul makhija <rahulmkhj@gmail.com>
2014-12-14 15:15:15 +0530

    added new method: initiate
    added new method to initiate a direcpay transaction.

--- laravel/app/models/admin/PaymentGateways/DirecpayTransaction.php
+++ laravel/app/models/admin/PaymentGateways/DirecpayTransaction.php
@@ -10,5 +10,32 @@ class DirecpayTransaction extends BaseModel {
 	{
 		return $this->morphMany('OnlinePayment','gw');
 	}
+
+	public static function initiate( $user_id, $amount, $other_details )
+	{
+		DB::transaction(function()use( $user_id, $amount, $other_details ){
+
+			$dp_transaction = new self([
+												'status'	=>		'Initiated',
+										 'other_details'	=>		$other_details,
+										]);
+			if( ! $dp_transaction->save() )	throw new Exception('Could not initiate transaction. Failed: Phase 1');
+
+			do{
+				$uid = uniqud();
+				$exists = OnlinePayment::where('order_id',$uid)->count();
+			} while( $exists );
+			$transaction = new OnlinePayment([
+												'user_id'	=>	$user_id,
+												'gw_type'	=>	'DIRECPAY',
+												  'gw_id'	=>	$dp_transaction->id,
+											   'order_id'	=>	$uid,
+												 'amount'	=>	$amount
+											]);
+				if( ! $transaction->save() )	throw new Exception('Could not initiate transaction. Failed: Phase 2');
+				
+				return $transaction->order_id;
+			});
+	}
 }
 //end of file DirecpayTransaction.php
