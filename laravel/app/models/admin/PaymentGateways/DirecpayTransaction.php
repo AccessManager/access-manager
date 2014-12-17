801f292bc097dc643f5060ea9604d3b9f79d5b8e
Rahul makhija <rahulmkhj@gmail.com>
2014-12-17 14:33:07 +0530

    number of fixes to DirecpayTransaction
    Online recharge with direcpay now works.

--- laravel/app/models/admin/PaymentGateways/DirecpayTransaction.php
+++ laravel/app/models/admin/PaymentGateways/DirecpayTransaction.php
@@ -1,5 +1,5 @@
 <?php
-
+use RAHULMKHJ\PaymentGateways\Direcpay\DirecpayResponse;
 class DirecpayTransaction extends BaseModel {
 
 	protected $table = 'direcpay_transactions';
@@ -11,23 +11,23 @@ class DirecpayTransaction extends BaseModel {
 		return $this->morphMany('OnlinePayment','gw');
 	}
 
-	public static function initiate( $user_id, $amount, $other_details )
+	public static function initiate( $user_id, $amount, Array $other_details )
 	{
-		DB::transaction(function()use( $user_id, $amount, $other_details ){
+		return DB::transaction(function()use( $user_id, $amount, $other_details ){
 
 			$dp_transaction = new self([
 												'status'	=>		'Initiated',
-										 'other_details'	=>		$other_details,
+										 'other_details'	=>		http_build_query($other_details),
 										]);
 			if( ! $dp_transaction->save() )	throw new Exception('Could not initiate transaction. Failed: Phase 1');
 
 			do{
-				$uid = uniqud();
+				$uid = uniqid();
 				$exists = OnlinePayment::where('order_id',$uid)->count();
 			} while( $exists );
 			$transaction = new OnlinePayment([
 												'user_id'	=>	$user_id,
-												'gw_type'	=>	'DIRECPAY',
+												'gw_type'	=>	'DirecpayTransaction',
 												  'gw_id'	=>	$dp_transaction->id,
 											   'order_id'	=>	$uid,
 												 'amount'	=>	$amount
@@ -36,6 +36,17 @@ class DirecpayTransaction extends BaseModel {
 				
 				return $transaction->order_id;
 			});
+	}
+
+	public static function succeed(DirecpayResponse $dp)
+	{
+		$response = $dp->getResponse();
+
+		$txn = OnlinePayment::where('order_id',$response['merchantordernumber'])->first();
+
+		$dpTxn = $txn->gw;
+		$dpTxn->fill(['status'=>$response['status'],'dp_refrence_id'=>$response['direcpayreferenceid'],]);
+		$dpTxn->save();
 	}
 }
 //end of file DirecpayTransaction.php
