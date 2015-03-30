93c350533e31825aced9530f69fb1b4698061a2e
Rahul makhija <rahulmkhj@gmail.com>
2015-03-30 19:22:49 +0530

    added tax_rate to fillables of APChangeHistory model.

--- laravel/app/models/admin/AdvancePaid/APChangeHistory.php
+++ laravel/app/models/admin/AdvancePaid/APChangeHistory.php
@@ -5,7 +5,7 @@ class APChangeHistory extends BaseModel {
 	protected $table = 'ap_change_history';
 	protected $fillable = ['user_id','from_date','to_date','plan_name','plan_type',
 						'limit_type','time_limit','time_unit','data_limit','data_unit',
-						'aq_access','aq_policy','price',];
+						'aq_access','aq_policy','price','tax_rate'];
 	public $timestamps = FALSE;
 }
 
