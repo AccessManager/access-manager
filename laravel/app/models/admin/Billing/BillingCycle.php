b56929fa7ffdad19f777670dbd2cb216d7528500
Rahul makhija <rahulmkhj@gmail.com>
2015-10-11 23:32:44 +0530

    added "bill_duration_type" to the fillables.

--- laravel/app/models/admin/Billing/BillingCycle.php
+++ laravel/app/models/admin/Billing/BillingCycle.php
@@ -3,6 +3,6 @@
 class BillingCycle extends BaseModel {
 	protected $table = 'billing_cycles';
 	protected $fillable = ['user_id','billing_cycle','billing_unit','org_id',
-							'last_billed_on','bill_date','expiration'];
+							'last_billed_on','bill_date','expiration','bill_duration_type'];
 	public $timestamps = FALSE;
 }
