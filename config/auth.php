d2d68ba2f98a474085a171f84c334cf187d8bff5
Rahul makhija <rahulmkhj@gmail.com>
2017-09-21 20:03:22 +0530

    updated correct namespace for User model.

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => \AccessManager\Auth\Models\User::class,
         ],
 
         // 'users' => [
